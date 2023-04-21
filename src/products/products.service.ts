328b0340580fa0e46d4d97cc24d1124470b8ecf2
luizpbello <luizpaulopinto25@gmail.com>
2023-04-20 23:05:32 -0300

    feat: creating route to add products

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -1,11 +1,23 @@
 import { Injectable } from '@nestjs/common';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
+import { InjectModel } from '@nestjs/mongoose';
+import { Product } from './entities/product.entity';
+import { Model } from 'mongoose';
 
 @Injectable()
 export class ProductsService {
-  create(createProductDto: CreateProductDto) {
-    return 'This action adds a new product';
+
+  constructor(
+    @InjectModel('Product') private readonly productModel: Model<Product>
+  ){}
+
+  async create(createProductDto: CreateProductDto) {
+
+    const newProduct = new this.productModel(createProductDto)
+    await newProduct.save()
+
+    
   }
 
   findAll() {
